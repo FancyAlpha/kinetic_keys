91a2b0624115039c86a18ba61102f1f1a4e3c8ac
Pranav Rayudu <tarakapranav@gmail.com>
2020-02-03 11:43:28 -0600

    polished front page

--- src/App.js
+++ src/App.js
@@ -1,60 +1,82 @@
 /* Import statements */
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
+
+import {fade} from '@material-ui/core';
+
 import AppBar from "@material-ui/core/AppBar";
-import {createMuiTheme} from '@material-ui/core/styles';
-import {ThemeProvider} from '@material-ui/core/styles';
+// import {createMuiTheme} from '@material-ui/core/styles';
+// import {ThemeProvider} from '@material-ui/core/styles';
 
 
 import Home from './routes/home';
 import Experiment from "./routes/experiment";
 
 import makeStyles from "@material-ui/core/styles/makeStyles";
-// import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
-// import Icon from '@mdi/react';
-// import {mdiHome} from '@mdi/js';
-// import {mdiTestTube} from '@mdi/js';
-import {Home as HomeIcon, TestTube} from 'mdi-material-ui';
 
+import {Home as HomeIcon, TestTube} from 'mdi-material-ui';
+import {Container} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 // import {red} from "@material-ui/core/colors";
 
 const useStyles = makeStyles(theme => ({
+        root: {
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+        },
 
-    iconButton: {
-        margin: theme.spacing(1, 2, 1, 2),
-        // border: "1px solid red",
-    },
-}));
+        nav: {
+            padding: theme.spacing(1, 0),
+        },
+
+        footer: {
+            background: fade(theme.palette.grey.A200, 0.2),
+            color: theme.palette.grey.A400,
+            paddingTop: '8px',
+            paddingBottom: '8px',
+        },
+    }))
+;
 
 /* App component */
 function App() {
 
-    // const styles = useStyles();
+    const styles = useStyles();
 
     return (
         <CssBaseline>
+            <div className={styles.root}>
+                <Toolbar variant={"dense"} className={styles.nav}>
+                    <Container>
+                        <Link to={"/"}>
+                            <IconButton edge="start" aria-label="menu">
+                                <HomeIcon/>
+                            </IconButton>
+                        </Link>
 
-            <AppBar>
-                <Toolbar>
-                    <IconButton edge="start" aria-label="menu">
-                        <HomeIcon color={"action"}/>
-                    </IconButton>
-
-                    <IconButton edge="start" aria-label="menu">
-                        <TestTube />
-                    </IconButton>
+                        <Link to={"/experiment"}>
+                            <IconButton aria-label="menu">
+                                <TestTube/>
+                            </IconButton>
+                        </Link>
+                    </Container>
                 </Toolbar>
-            </AppBar>
+                {/*</AppBar>*/}
 
-            <Route path="/" exact component={Home}/>
-            <Route path="/experiment" exact component={Experiment}/>
+                <Route path="/" exact component={Home}/>
+                <Route path="/experiment" exact component={Experiment}/>
 
-            <footer>
-                This is a footer
-            </footer>
+                <footer>
+                    <Container className={styles.footer}>
+                        <Typography variant={"caption"}>&copy; 2020 Kinetic Keys project by Mehul
+                            Daruka, Mayank Daruka, and Pranav Rayudu.</Typography>
+                    </Container>
+                </footer>
+            </div>
         </CssBaseline>
     );
 }
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from '../logo.svg';
 import './Home.css';
 
 import {makeStyles} from '@material-ui/core/styles';
@@ -23,22 +22,18 @@ export default function Home() {
     const classes = useStyles();
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <Typography variant="h1">Kinetic Keys</Typography>
-                <Typography variant="subtitle1">Type words with your body!</Typography>
-                <Button variant="contained"
-                        color="secondary"
-                        endIcon={<SendIcon/>}
-                        className={classes.spaced}
-                        component={Link}
-                        to="/experiment">
-                    Play the Game!
-                </Button>
-            </header>
-
-            {/*<Route path="/"/>*/}
+        <header className="App-content">
+            <Typography variant="h1" component={"h1"}>Kinetic Keys</Typography>
+            <Typography variant="subtitle1">Type words with your body!</Typography>
+            <Button variant="contained"
+                    color="secondary"
+                    endIcon={<SendIcon/>}
+                    className={classes.spaced}
+                    component={Link}
+                    to="/experiment">
+                Play the Game!
+            </Button>
             <Route path="/experiment" component={Experiment}/>
-        </div>
+        </header>
     );
 }
