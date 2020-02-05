1cf5f425ef774714064d5f7fa258616db2d5a9d7
Pranav Rayudu <tarakapranav@gmail.com>
2020-02-05 00:37:54 -0600

    Redid experiment page grid layout using CSS grid

--- src/routes/experiment.js
+++ src/routes/experiment.js
@@ -1,53 +1,73 @@
 import React from 'react';
+import "../styles/styles.css";
 
-import "./styles.css";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import { Container, CardContent } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
+import {CardContent} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
-    card: {
-        margin: theme.spacing(1,1,1,1),
-        display: 'block'
+
+    bodyContainer: {
+        display: 'grid',
+        gridTemplateColumns: "1fr 1fr",
+        gridTemplateRows: "60% 1fr",
+
+        gridGap: theme.spacing(2),
+        padding: theme.spacing(2),
+        placeContent: "stretch",
     },
+
+    letterSection: {
+        gridColumn: "1 / span 1",
+        gridRow: "1 / span 1",
+    },
+
+    cameraSection: {
+        gridColumn: "2 / span 1",
+        gridRow: "1 / span 1",
+    },
+
+    wordSection: {
+        gridColumn: "1 / span 2",
+        gridRow: "2 / span 1",
+    }
 }));
 
 function Experiment() {
     const classes = useStyles();
 
     return (
-        <Grid container className="Main-content" spacing = {4}>
-            <Grid item xs={12} sm={6}>
-                <Card style = {{backgroundColor: 'rgb(247, 195, 195)' }}> 
+        <div className={[classes.bodyContainer, "Main-content"].join(" ")}>
+            <Card className={classes.letterSection}>
+                {/*<Card>*/}
                     <CardContent>
                         <Typography>
                             This is another item (Card)
                         </Typography>
                     </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <Card style = {{backgroundColor: 'rgb(145, 115, 206)'}}> 
+                {/*</Card>*/}
+            </Card>
+            <Card className={classes.cameraSection}>
+                {/*<Card>*/}
                     <CardContent>
                         <Typography>
                             This is another item (Card)
                         </Typography>
                     </CardContent>
-                </Card>
-            </Grid>
-            <Grid item xs={12}>
-                <Card style = {{backgroundColor: 'rgb(154, 228, 203)'}}> 
+                {/*</Card>*/}
+            </Card>
+
+            <Card className={classes.wordSection}>
+                {/*<Card>*/}
                     <CardContent>
                         <Typography>
                             This is another item (Card)
                         </Typography>
                     </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
+                {/*</Card>*/}
+            </Card>
+        </div>
     );
 }
 
